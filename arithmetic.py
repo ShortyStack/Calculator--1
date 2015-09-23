a80eeeb011184933353f655a465cd64f5f46bb22
Hackbright Student <info@hackbrightacademy.com>
2015-09-23 15:45:09 -0700

    first commit on working calculator1

---
+++ arithmetic.py
@@ -0,0 +1,36 @@
+def add(num1, num2):
+    total = num1 + num2
+    return total
+
+def subtract(num1, num2):
+    minus = num1 - num2
+    return minus
+
+def multiply(num1, num2):
+    multiple = num1 * num2
+    return multiple
+
+def divide(num1, num2):
+    division = num1 / num2
+    return division
+    
+
+def square(num1):
+    sq = num1 ** 2
+    return sq
+
+def cube(num1):
+    raise_to_three = num1 ** 3
+    return raise_to_three
+    
+
+def power(num1, num2):
+    find_power = num1 ** num2
+    return find_power
+
+def mod(num1, num2):
+    modulo = num1 % num2
+    return modulo
+
+    
+
